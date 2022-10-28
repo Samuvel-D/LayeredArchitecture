2f73e2646dcb581bf1d07f71fe499ceeac194978
Samuvel-D <samuvel.d1999@gmail.com>
2022-10-28 15:07:56 +0530

    ExceptionChanged

--- src/com/Bu/Invent/Bussiness/BussinessException.java
+++ src/com/Bu/Invent/Bussiness/BussinessException.java
@@ -3,6 +3,7 @@ package com.Bu.Invent.Bussiness;
 public class BussinessException extends Exception {
     public BussinessException(String Exception){
         super(Exception);
+        System.out.println("ihdeihd");
     }
 
 }
